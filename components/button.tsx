70f82e268433636af5851bc273e6caf17add0bfb
mlngwan <david027301@naver.com>
2025-03-30 21:33:32 +0900

    Button Components : 분석중 상태

---
+++ components/button.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+interface ButtonProops {
+  text: string;
+  loading: boolean;
+}
+export default function Button({ text, loading }: ButtonProops) {
+  return (
+    <button
+      disabled={loading}
+      className="w-full h-10 mt-3 cursor-pointer bg-neutral-300 hover:bg-neutral-200 text-black transition-colors 
+    font-semibold rounded-2xl shadow-md disabled:bg-neutral-400 dark:bg-neutral-100 dark:hover:bg-neutral-50 dark:text-black"
+    >
+      {!loading ? text : "분석중..."}
+    </button>
+  );
+}
